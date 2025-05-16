39dcd4a3dc9b9a3ec4b9229591f494a612ab3ae4
absalpha <doeua157@gmail.com>
2025-05-15 21:14:08 -0700

    about project

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,13 +13,22 @@ export default function AboutPage() {
       {/* Placeholder text block */}
       <div className="max-w-3xl text-center mb-16">
         <p>
-
-This demo for The Robot Underground was developed as part of our year-long computer science capstone project for Oregon State University! The six of us joined forces with one mission: to build a demo for a robotics video game the likes of which the world has never seen before.
-
-Our development journey was not for the faint of heart—we raced up the Unreal Engine learning curve, designed game mechanics from scratch, and created art in-house so that we could bring our vision to life. Below is a timeline of this journey, for your enjoyment.
-
-If you want to follow the next chapter of The Robot Underground (spoiler alert: it’s pretty exciting), then follow us on all the socials.
-
+          This demo for The Robot Underground was developed as part of our
+          year-long computer science capstone project for Oregon State
+          University! The six of us joined forces with one mission: to build a
+          demo for a robotics video game the likes of which the world has
+          never seen before.
+        </p>
+        <p>
+          Our development journey was not for the faint of heart—we raced up the
+          Unreal Engine learning curve, designed game mechanics from scratch,
+          and created art in-house so that we could bring our vision to life.
+          Below is a timeline of this journey, for your enjoyment.
+        </p>
+        <p>
+          If you want to follow the next chapter of The Robot Underground
+          (spoiler alert: it’s pretty exciting), then follow us on all the
+          socials.
         </p>
       </div>
 
